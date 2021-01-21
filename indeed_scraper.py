8653976ae8cb4e36cba628a8e7918c66d601fcc3
Steve Clarke <srclarke97@gmail.com>
2021-01-21 09:06:36 +0800

    new basic structure

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -6,40 +6,49 @@ import selenium
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
-from web_extension import get_job_descriptions
 
 def find_jobs():
-    job_title = input("Enter job title: ")
-    location = input("Enter location: ")
+    countrycode     = input("Enter country prefix (eg. au, sg, hk): ")
+    locality        = input("Enter locality: ")
+    job_title       = input("Enter job title: ")
+    num_pages       = input("Number of pages to be searched: ")
 
-    jobs, url               = scan_listings(job_title, location)
-    jobs_list, n_listings   = extract_job_information(jobs, url)
+    urls        = get_urls(countrycode, locality, job_title, num_pages)
+    jobs        = get_jobs(urls)
+    jobs_data, n_listings   = extract_data(jobs)
     print('{} new job postings retrieved.'.format(n_listings))
 
-    save_to_excel(jobs_list, "results.xlsx")
+    save_to_excel(jobs_data, "results.xlsx")
 
 ## -- save to excel -- ##
-def save_to_excel(jobs_list, filename):
-    jobs = pd.DataFrame(jobs_list)
+def save_to_excel(jobs_data, filename):
+    jobs = pd.DataFrame(jobs_data)
     jobs.to_excel(filename)
 
-## -- Get listing data and make a soup -- ##
-def scan_listings(job_title, location):
-    job_title   = job_title.replace(" ", "+")
+def get_urls(countrycode, locality, job_title, num_pages):
+    num_pages   = int(str(num_pages) + str('0'))
 
-    print(job_title)
-    print(location)
+    urls = []
 
-    pages = np.arange(0, 10, 10) # set the second argument to scan multiple pages. '10' defaults to first page only
+    pages = np.arange(0, num_pages, 10) # set the second argument to scan multiple pages. '10' defaults to first page only
     for page in pages:
         url_variables   = {'q' : job_title,
-                        'l' : location,
+                        'l' : locality,
                         'fromage' : 'last',
                         'sort' : 'date', 
                         'start': page
                         }
+        url = ('https://' + countrycode + '.indeed.com/jobs?' + urllib.parse.urlencode(url_variables))
+        print(url)
+        urls.append(url)
 
-        url     = ('https://au.indeed.com/jobs?' + urllib.parse.urlencode(url_variables))
+    print(*urls, sep='\n')
+    return urls
+
+## -- Get listing data and make a soup -- ##
+def get_jobs(urls):
+
+    for url in urls:
 
         print('scanning {} ...'.format(url))
         page    = requests.get(url)
@@ -50,7 +59,35 @@ def scan_listings(job_title, location):
         except NameError: # ...else, create it
             jobs = soup.find(id="resultsCol")
 
-    return jobs, url
+    return jobs
+
+def extract_data(jobs):
+    job_elems = jobs.find_all('div', class_='jobsearch-SerpJobCard')
+    
+    cols            = []
+    extracted_info  = []
+    
+    titles          = []
+    companies       = []
+
+    cols.append('titles')
+    for job_elem in job_elems:
+        titles.append(get_job_title(job_elem))
+    extracted_info.append(titles)
+
+    cols.append('companies')
+    for job_elem in job_elems:
+        companies.append(get_company_name(job_elem))
+    extracted_info.append(companies)
+    
+    jobs_list = {}
+    
+    for j in range(len(cols)):
+        jobs_list[cols[j]] = extracted_info[j]
+    
+    n_listings = len(extracted_info[0])
+    
+    return jobs_list, n_listings
 
 ## --- functions for handling data --- ##
 def get_job_title(job_elem) :
@@ -62,37 +99,3 @@ def get_company_name(job_elem) :
     company_elem    = job_elem.find('div', class_='sjcl')
     company         = company_elem.text.strip()
     return company
-
-## --- extract data --- ##
-def extract_job_information(jobs, url):
-    job_elems = jobs.find_all('div', class_='jobsearch-SerpJobCard')
-    
-    cols            = []
-    extracted_info  = []
-    
-    titles          = []
-    companies       = []
-    descriptions    = []
-
-    cols.append('titles')
-    for job_elem in job_elems:
-        titles.append(get_job_title(job_elem))
-    extracted_info.append(titles)
-
-    cols.append('companies')
-    for job_elem in job_elems:
-        companies.append(get_company_name(job_elem))
-    extracted_info.append(companies)
-
-    cols.append('descriptions')
-    descriptions = get_job_descriptions(url, descriptions)
-    extracted_info.append(descriptions)
-    
-    jobs_list = {}
-    
-    for j in range(len(cols)):
-        jobs_list[cols[j]] = extracted_info[j]
-    
-    n_listings = len(extracted_info[0])
-    
-    return jobs_list, n_listings
