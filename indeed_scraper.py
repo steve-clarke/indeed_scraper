d80d59eda9a9445b093b76d2c7148cdca4560cd3
Steve Clarke <srclarke97@gmail.com>
2021-01-21 09:25:48 +0800

    merged source files

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -3,9 +3,17 @@ import requests
 import os
 import openpyxl
 import selenium
+import json
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException, NoSuchElementException
+
 
 def find_jobs():
     countrycode     = input("Enter country prefix (eg. au, sg, hk): ")
@@ -15,15 +23,17 @@ def find_jobs():
 
     urls        = get_urls(countrycode, locality, job_title, num_pages)
     jobs        = get_jobs(urls)
-    jobs_data, n_listings   = extract_data(jobs)
+
+    jobs_data, n_listings = extract_data(jobs, urls)
     print('{} new job postings retrieved.'.format(n_listings))
 
     save_to_excel(jobs_data, "results.xlsx")
 
-## -- save to excel -- ##
+
 def save_to_excel(jobs_data, filename):
     jobs = pd.DataFrame(jobs_data)
     jobs.to_excel(filename)
+
 
 def get_urls(countrycode, locality, job_title, num_pages):
     num_pages   = int(str(num_pages) + str('0'))
@@ -45,7 +55,7 @@ def get_urls(countrycode, locality, job_title, num_pages):
     print(*urls, sep='\n')
     return urls
 
-## -- Get listing data and make a soup -- ##
+
 def get_jobs(urls):
 
     for url in urls:
@@ -61,7 +71,8 @@ def get_jobs(urls):
 
     return jobs
 
-def extract_data(jobs):
+
+def extract_data(jobs, urls):
     job_elems = jobs.find_all('div', class_='jobsearch-SerpJobCard')
     
     cols            = []
@@ -69,6 +80,7 @@ def extract_data(jobs):
     
     titles          = []
     companies       = []
+    descriptions    = []
 
     cols.append('titles')
     for job_elem in job_elems:
@@ -79,6 +91,11 @@ def extract_data(jobs):
     for job_elem in job_elems:
         companies.append(get_company_name(job_elem))
     extracted_info.append(companies)
+
+    cols.append('descriptions')
+    for url in urls:
+        descriptions = get_job_descriptions(url, descriptions)
+    extracted_info.append(descriptions)
     
     jobs_list = {}
     
@@ -89,13 +106,55 @@ def extract_data(jobs):
     
     return jobs_list, n_listings
 
-## --- functions for handling data --- ##
+
 def get_job_title(job_elem) :
     title_elem  = job_elem.find('h2', class_='title')
     title       = title_elem.text.strip()
     return title
 
+
 def get_company_name(job_elem) : 
     company_elem    = job_elem.find('div', class_='sjcl')
     company         = company_elem.text.strip()
     return company
+
+
+# driver setup for job description crawler
+# currently only supports Chrome 87.0.4280
+def driver_setup():
+    with open('config/driver_window.json') as window:
+        minimised = json.load(window).get('minimised')
+    driver = webdriver.Chrome('config/chromedriver')
+    if minimised:
+        driver.minimize_window()
+    else:
+        driver.maximize_window()
+
+    return driver
+
+def get_job_descriptions(url, descriptions):
+    new_descriptions = descriptions
+    driver = driver_setup()
+    wait = WebDriverWait(driver, 10)
+
+    driver.get(url)
+    wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'jobsearch-SerpJobCard')))
+    jobs = driver.find_elements_by_class_name('jobsearch-SerpJobCard')
+
+    def click_on_job_and_add_description(job_card):
+        job_card.click()
+        wait.until(EC.presence_of_element_located((By.ID, 'vjs-content')))
+        new_descriptions.append(driver.find_element_by_id('vjs-content').text)
+
+    for job in jobs:
+        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'jobsearch-SerpJobCard')))
+        try: 
+            click_on_job_and_add_description(job)
+        except (ElementClickInterceptedException, TimeoutException):
+            # make another attempt after window scroll
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            click_on_job_and_add_description(job)
+
+    driver.close()
+    driver.quit()
+    return new_descriptions
